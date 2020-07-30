f51a39800ade708c12b0dec12732180bd626d6b4
chip <chiposaur@gmail.com>
2020-07-30 16:40:55 -0500

    touched by FE

---
+++ README.md
@@ -0,0 +1,11 @@
+# cvMan
+
+## Front
+* create-react-app
+* ionic framework UI library
+* recoil state management
+
+## Back
+* express
+* knex
+* postgres
--- routes/buildRouter.js
+++ routes/buildRouter.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+	res.status(200).json({ butt: "buildRouter/" })
+	console.log(6, );
+})
+
 module.exports = router;
